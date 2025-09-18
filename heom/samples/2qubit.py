896402d2acd1f1d6fad1a92b9deb3ae509789cd6
KiNakamura <nakamura.kiyoto.n20@kyoto-u.jp>
2025-09-18 19:16:54 +0200

    Samples for 2qubit systems

---
+++ heom/samples/2qubit.py
@@ -0,0 +1,60 @@
+from qiskit import QuantumCircuit
+import numpy as np
+from .. import main
+from ..pulse.rxy_step import rxyStep
+from ..pulse.direct_cpl_step_varJ import directCplStepVarJ
+
+def run():
+    fileName = 'rdo_2qubit.csv'
+
+    qc = QuantumCircuit(2)
+    qc.ry(-0.5*np.pi, 1)
+    qc.rx(0.5*np.pi, 0)
+    qc.iswap(0, 1)
+    qc.ry(-0.5*np.pi, 1)
+    qc.iswap(0, 1)
+
+    idlingTime = 0.0
+
+    rho = {'numQ': 2}
+    rho['rhoIni'] = np.array([[1, 0, 0, 0],
+                              [0, 0, 0, 0],
+                              [0, 0, 0, 0],
+                              [0, 0, 0, 0]], dtype=np.complex128)
+    
+    rho['omegaQ'] = [1., 1.] # in the order: [qubit 1, qubit 0]
+
+    kwargs1Q = [{'omega': -rho['omegaQ'][0], 'gateTime': 0.1},
+                {'omega': -rho['omegaQ'][1], 'gateTime': 0.1},]
+    
+    kwargs2Q = {'gateTime': 0.05}
+
+    pulse = [[[0], rxyStep(**kwargs1Q[0])],
+             [[1], rxyStep(**kwargs1Q[1])],
+             [[0, 1], directCplStepVarJ(**kwargs2Q)]]
+    
+    map = {(np.int64(0),): 0,
+           (np.int64(1),): 1,
+           (np.int64(0), np.int64(1)): 2}
+
+    bath = ['s=1', 's=1'] # in the order: [qubit 1, qubit 0]
+
+    V = np.array([
+        [[0, 1],
+         [1, 0]],
+
+        [[0, 1],
+         [1, 0]]
+    ], dtype=np.complex128) # in the order: [qubit 1, qubit 0]
+
+    dtFB = 0.001
+
+    strideTime = 0.01
+
+    stride = int(strideTime // dtFB)
+
+    main(fileName, qc, idlingTime, pulse, map, rho,
+         bath, V, dtFB, stride)
+    
+if __name__ == '__main__':
+    run()
