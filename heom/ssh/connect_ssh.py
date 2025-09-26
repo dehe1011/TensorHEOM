51cc82b67846b76b475f7efd3ce9ce99018b4d9f
KiNakamura <nakamura.kiyoto.n20@kyoto-u.jp>
2025-09-26 19:36:29 +0200

    For specific SSH connection (JUSTUS2)

--- heom/ssh/connect_ssh.py
+++ heom/ssh/connect_ssh.py
@@ -1,24 +1,31 @@
+"""For JUSTUS2
+
+"""
 import socket
 import paramiko
-import getpass
 
-def handler(title, instructions, prompt_list):
-    answers = []
+def handlerWithValues(password, otp):
+    def handler(title, instructions, prompt_list):
+        answers = []
 
-    for pr in prompt_list:
-        query = pr[0]
-        ans = getpass.getpass(query)
+        for pr in prompt_list:
+            query = pr[0]
+            if query == 'Your OTP:':
+                answers.append(otp)
+            elif query == 'Password:':
+                answers.append(password)
 
-        answers.append(ans)
+        return answers
+    return handler
 
-    return answers
-
-def getClient(hostname, username):
+def getClient(hostname, username, password, otp):
     """get Client of paramiko
 
         params:
             hostname (str): server name to connect to
             username (str): user name
+            password (str): password
+            otp (str): one-time password
 
         returns:
             client (paramiko.client.SSHClient)
@@ -31,7 +38,7 @@ def getClient(hostname, username):
     sock.connect((hostname, 22))
     ts = paramiko.Transport(sock)
     ts.start_client()
-    ts.auth_interactive(username, handler)
+    ts.auth_interactive(username, handlerWithValues(password, otp))
 
     client._transport = ts
 
---
+++ heom/ssh/connect_ssh_deprecated.py
@@ -0,0 +1,44 @@
+"""Deprecated.
+    This module is kept for potential future use.
+
+    Users can enter parameter values (password, OTP, etc.) interactively.
+"""
+
+import socket
+import paramiko
+import getpass
+
+def handler(title, instructions, prompt_list):
+    answers = []
+
+    for pr in prompt_list:
+        query = pr[0]
+        ans = getpass.getpass(query)
+
+        answers.append(ans)
+
+    return answers
+
+def getClient(hostname, username):
+    """get Client of paramiko
+
+        params:
+            hostname (str): server name to connect to
+            username (str): user name
+
+        returns:
+            client (paramiko.client.SSHClient)
+    """
+
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((hostname, 22))
+    ts = paramiko.Transport(sock)
+    ts.start_client()
+    ts.auth_interactive(username, handler)
+
+    client._transport = ts
+
+    return client
