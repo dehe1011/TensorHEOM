915c9fd1c55f439c41d436dc615773f576a91775
Dennis Herb <dennis.herb@uni-ulm.de>
2025-10-06 14:09:26 +0200

    fidelity and concurrence plotting

--- heom/gui/plotting_window.py
+++ heom/gui/plotting_window.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 import customtkinter as ctk
 import matplotlib.pyplot as plt
+from qiskit.quantum_info import Operator
+from scipy.linalg import eigvals
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # ----------------------------------------------------------------------
@@ -48,14 +50,54 @@ class PlottingWindow(ctk.CTkToplevel):
             row=0, column=0, columnspan=2, padx=10, pady=10, sticky="nsew"
         )
 
-        if self.controller.plot_kwargs['plot_type'] == 'Full density matrix':
-            self.plot_full_dm()
+        if self.controller.plot_kwargs['plot_type'] == 'Density matrix':
+            self.plot_dm()
+
+        if self.controller.plot_kwargs['plot_type'] == 'Fidelity':
+            self.plot_fidelity()
+        
+        if self.controller.plot_kwargs['plot_type'] == 'Concurrence':
+            if not self.controller.kwargs['numQ'] == 2:
+                print("Concurrence is only defined for 2 qubits.")
+                return
+            else:
+                self.plot_concurrence()
 
         self.plotting(self.fig)
 
     # ------------------------------------------------------------------
 
-    def plot_full_dm(self):
+    def plot_concurrence(self):
+                  
+        sigma_y = np.array([[0, -1j], [1j, 0]])
+        Y = np.kron(sigma_y, sigma_y)
+
+        concs = []
+        for rho in self.controller.dm_list:
+            rho_tilde = Y @ rho.conj() @ Y
+            R = rho @ rho_tilde
+            eigenvals = np.sort(np.sqrt(np.abs(eigvals(R))))[::-1]
+            C = max(0, eigenvals[0] - sum(eigenvals[1:]))
+            concs.append(C)
+
+        self.fig, self.ax = plt.subplots()
+        self.ax.plot(self.controller.t_list, concs)
+        self.ax.set_xlabel("Time [ns]")
+        self.ax.set_ylabel("Concurrence")
+        self.ax.set_ylim(0, 1.05)
+
+    def plot_fidelity(self):
+        U = Operator(self.controller.qc).data
+        target = U @ self.controller.rho['rhoIni'] @ U.conj().T
+        fids = [np.real(np.trace(rho @ target)) for rho in self.controller.dm_list]
+
+        self.fig, self.ax = plt.subplots()
+        self.ax.plot(self.controller.t_list, fids)
+        self.ax.set_xlabel("Time [ns]")
+        self.ax.set_ylabel("Fidelity")
+        self.ax.set_ylim(0, 1.05)
+
+    def plot_dm(self):
         dim = self.controller.dm_list[0].shape[0]
         self.fig, self.ax = plt.subplots(dim, dim, figsize=(dim*1.7, dim*1.05), sharex=True, sharey=True)
         for i in range(dim):
