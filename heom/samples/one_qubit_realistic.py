baebe14907768f6ab29db1708cbdabdd1edc8fb7
KiNakamura <nakamura.kiyoto.n20@kyoto-u.jp>
2025-11-19 18:35:22 +0900

    A sample for single-qubit systems with realistic parameter values

---
+++ heom/samples/one_qubit_realistic.py
@@ -0,0 +1,64 @@
+import getpass
+from qiskit import QuantumCircuit
+import numpy as np
+from ..ssh.job_control import submitJob
+
+def run1():
+    qc = QuantumCircuit(1)
+    qc.rx(0.5*np.pi, 0)
+    qc.delay(0, 0)
+    qc.ry(0.5*np.pi, 0)
+    qc.delay(0, 0)
+    qc.ry(-0.5*np.pi, 0)
+
+    idlingTime = 10*np.pi # 1 ns
+
+    rho = {'numQ': 1}
+    rho['rhoIni'] = np.array([[1, 0],
+                              [0, 0]], dtype=np.complex128)
+    rho['omegaQ'] = [1.]
+
+    gateList = [[[0], 'rxyStep', ]]
+    # gate time: 16 ns
+    kwargs1Q = {'omega': -rho['omegaQ'][0], 'gateTime': 160 * np.pi}
+    gateList = [[[0], 'rxyStep', kwargs1Q]]
+
+    bath = [{'type': 'broadband'}]
+    bath[0]['beta'] = 8 # 30 mK
+    bath[0]['kappa'] = 1e-6 / 2 / np.pi # T1: 32 us
+    bath[0]['omegaC'] = 50
+    bath[0]['exp'] = 1
+    bath[0]['tol'] = 1e-6
+
+    V = np.array([
+        [[0, 1],
+         [1, 0]]
+    ], dtype=np.complex128)
+
+    dtFB = 0.005 # 0.001
+
+    strideTime = 3
+
+    stride = int(strideTime / dtFB)
+
+    depth = [1]
+    bondDim = 5
+
+    submissionParams = {}
+    submissionParams['hostname'] = 'justus2.uni-ulm.de'
+    submissionParams['schedulerName'] = 'slurm'
+    submissionParams['numNodes'] = 1
+    submissionParams['cpusPerTask'] = 4
+    submissionParams['maxTime'] = '336:00:00'
+    submissionParams['others'] = ''
+    submissionParams['venvPath'] = '$HOME/.heom'
+
+    submissionParams['username'] = input('User Name: ')
+    submissionParams['emailAddress'] = input('Email Address: ')
+    submissionParams['otp'] = getpass.getpass('Your OTP: ')
+    submissionParams['password'] = getpass.getpass('Password: ')
+    submitJob(submissionParams, qc, idlingTime, gateList, rho,
+              bath, V, dtFB, stride, depth, bondDim, useRFPlus=True)
+    
+if __name__ == '__main__':
+    run1()
