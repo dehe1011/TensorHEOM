de82d2b5b7c439c4413f078a6a84634e0f5a23dd
Dennis Herb <dennis.herb@uni-ulm.de>
2026-06-22 17:59:28 +0200

    renaming: heom -> ttheom

--- ttheom/__init__.py
+++ ttheom/__init__.py
@@ -1,11 +1,11 @@
 # pylint: disable=wrong-import-position
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
 import pathlib
 import os
 
 ROOT_DIR = str(pathlib.Path(__file__).absolute().parent.parent)
-DATA_DIR = os.path.join(ROOT_DIR, "heom", "data")
+DATA_DIR = os.path.join(ROOT_DIR, "ttheom", "data")
 
 from .utils import *
 
